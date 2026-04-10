bf186a259d2a62e641c7576e4a05b7e9c6496104
Sasha T Heyburgh <sasha.heyburgh@gmail.com>
2026-04-10 13:46:58 +0200

    added function to log array values

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.topsoccerteams
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,28 +9,44 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Arrays
 
+    //created function to display array values in terminal
+    fun logArrValues(arr: Array<String>){
+        Log.v("Array values: ", Arrays.toString(arr))
+    }
+
 class MainActivity : AppCompatActivity() {
 
     //created array to hold top 5 soccer teams
     val teams = arrayOf<String>("Mam Sundowns FC", "Orlando Pirates", "Bidvest Wits",
         "Stellenbosch FC", "Sek United FC")
 
+    //function here ---------------------------------------------------------------
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        //function here ------------------------------------------------------------
         val teamsTxt = findViewById<TextView>(R.id.teamsDisplayId)
         var teamsDisplay = ""
         var count = 0
+
+        //calling logArrValues function
+        logArrValues(teams)
 
         //reassigning position 0 of the teams array
         teams[0] = "Mam Sundowns FC :)"
 
         //while loop to iterate through the teams array and display them
-        while(count < teams.count()){
-            teamsDisplay += "${teams[count]}\n"
-            count++
+//        while(count < teams.count()){
+//            teamsDisplay += "${teams[count]}\n"
+//            count++
+//        }
+
+        //for loop to iterate through the teams array and display them
+        for (team in teams){
+            teamsDisplay += "${teams}\n"
         }
 
         //displaying top 5 soccer teams in text view on UI
